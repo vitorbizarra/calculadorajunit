7f0ab822e8a83ead068ba671fb1e09a82fc790c8
vitorbizarra <vitorbizarracontato@gmail.com>
2023-09-16 20:08:22 -0300

    :construction: Classe Calculadora

---
+++ src/main/java/com/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+package com.calculadora;
+
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public double multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public double dividir(int a, int b) {
+        return a / b;
+    }
+}
